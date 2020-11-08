f004fd9ae432f2bb11a56f964f8ca32a1105937b
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-08 23:27:33 +0000

    Uploading project files to GitHub.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import random
+from random import randint, seed, choice
+
+
 # 🚨 Don't change the code below 👇
 test_seed = int(input("Create a seed number: "))
-random.seed(test_seed)
+seed(test_seed)
 
 # Split string method
-namesAsCSV = input("Give me everybody's names, seperated by a comma. ")
+namesAsCSV = input("Give me everybody's names, seperated by a comma.\n")
 names = namesAsCSV.split(", ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-print(names)
 
+# Solution 1:
+random_person = randint(0, len(names) -1)
+person_paying = names[random_person]
+print(f"{person_paying} is the person paying the bill.")
 
-'''
- Who's Paying program which will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
-'''
+print("")
+# Solution 2:
+
+paying_person = choice(names)
+print(f"{paying_person} is the person paying the bill.")
